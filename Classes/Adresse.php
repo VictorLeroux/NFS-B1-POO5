732c703f7bb16be8638234de981281f2c24182b0
Victor LEROUX <contact@fredericnoel.com>
2020-02-11 15:34:53 +0100

    ttttt

---
+++ Classes/Adresse.php
@@ -0,0 +1,16 @@
+<?php
+namespace Classes;
+
+class Aresse  
+{
+    public $numero;
+    public $rue;
+    public $cp;
+    public $ville; 
+    public $pays; 
+    
+    public function __construct ($numero,$rue,$cp,$ville,$pays)
+    {
+        
+    }
+}
